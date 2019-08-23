f622e481179944f7d85a55c04558863ab3acf32f
ceofred <johnsonmessilo19@gmail.com>
2019-08-23 10:39:56 +0100

    circle ci init

--- src/containers/auth/Register.jsx
+++ src/containers/auth/Register.jsx
@@ -8,14 +8,25 @@ import Google from "../../components/Partials/Auth/Button/Google";
 import Button from "../../components/Partials/Button";
 import Input from "../../components/Partials/Input";
 
-const emailIconSvg = (
-  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-at-sign">
-  <circle cx="12" cy="12" r="4"></circle>
-  <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94">
-  </path>
-  </svg>
-)
+import Helmet from "../../components/SEO/helmet";
 
+const emailIconSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="feather feather-at-sign"
+  >
+    <circle cx="12" cy="12" r="4" />
+    <path d="M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94" />
+  </svg>
+);
 
 const initialPrependsvg = (
   <svg
@@ -34,7 +45,7 @@ const initialPrependsvg = (
   </svg>
 );
 
-const finalAppendsvg = (
+const userIconSvg = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -45,69 +56,48 @@ const finalAppendsvg = (
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
-    class="feather feather-eye"
+    class="feather feather-user"
   >
-    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-    <circle cx="12" cy="12" r="3" />
+    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+    <circle cx="12" cy="7" r="4" />
   </svg>
 );
-
-const userIconSvg = (
-  <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="1em"
-                          height="1em"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="feather feather-user"
-                        >
-                          <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
-                          <circle cx="12" cy="7" r="4" />
-                        </svg>
-)
 export default function Register() {
   return (
     <div>
+      <Helmet title="Signup to colab" metaDescription="" />
+
       <section>
         <div class="mt-3 row align-items-center justify-content-center min-vh-100">
           <div class="col-md-6 col-lg-5 col-xl-4 py-6 py-md-0">
             <div>
               <div class="mb-5 text-center">
-                <h6 class="h3 mb-1">
-                Create your account
-                </h6>
-                <p class="text-muted mb-0">
-                  Made with love for developers
-                </p>
+                <h6 class="h3 mb-1">Create your account</h6>
+                <p class="text-muted mb-0">Made with love for developers</p>
               </div>
               <span class="clearfix" />
               <form>
-                 {/* email input */}
-                 <Input 
-                type="text"
-                placeholder="superuser"
-                label="username"
-                initialPrepend
-                initialPrependsvg={userIconSvg}
-                value=""
+                {/* email input */}
+                <Input
+                  type="text"
+                  placeholder="superuser"
+                  label="username"
+                  initialPrepend
+                  initialPrependsvg={userIconSvg}
+                  value=""
                 />
 
                 {/* email input */}
-                <Input 
-                type="email"
-                placeholder="someone@someserver.com"
-                label="Email address"
-                initialPrepend
-                initialPrependsvg={emailIconSvg}
-                value=""
+                <Input
+                  type="email"
+                  placeholder="someone@someserver.com"
+                  label="Email address"
+                  initialPrepend
+                  initialPrependsvg={emailIconSvg}
+                  value=""
                 />
                 {/* pasword input */}
                 <Input
-                  
                   type="password"
                   placeholder="Secret password"
                   label="password"
@@ -116,9 +106,8 @@ export default function Register() {
                   initialPrependsvg={initialPrependsvg}
                   value=""
                 />
-                                {/* confirm pasword input */}
-                                <Input
-                  
+                {/* confirm pasword input */}
+                <Input
                   type="password"
                   placeholder="Password again"
                   label="confirm password"
@@ -128,15 +117,14 @@ export default function Register() {
                   value=""
                 />
 
-
                 {/* checkbox */}
-                <Input fieldtype='checkbox'>
-                I agree to the <Link to="/public/terms">terms and conditions</Link>
+                <Input fieldtype="checkbox">
+                  I agree to the{" "}
+                  <Link to="/public/terms">terms and conditions</Link>
                 </Input>
 
                 <div class="mt-4">
-                  <Button type="button"
-                   textColor="#fff" block color="primary">
+                  <Button type="button" textColor="#fff" block color="primary">
                     Create my account
                   </Button>
                 </div>
