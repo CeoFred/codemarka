5a36d45e9cd55b49e68ffe6d105a2662b290dcb5
ceofred <johnsonmessilo19@gmail.com>
2019-09-30 06:30:31 +0100

    fix tet

--- src/index.js
+++ src/index.js
@@ -8,15 +8,21 @@ import * as serviceWorker from './serviceWorker';
 
 import store from './redux/store';
 
-export const app = (
+// export const app = (
+//     <Provider store={store}>
+//     <BrowserRouter>
+//     <App/>
+//     </BrowserRouter>
+//     </Provider>
+// );
+
+ReactDOM.render((
     <Provider store={store}>
     <BrowserRouter>
     <App/>
     </BrowserRouter>
     </Provider>
-);
-
-ReactDOM.render(app, document.getElementById('root') || document.createElement('div'));
+), document.getElementById('root') || document.createElement('div'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
