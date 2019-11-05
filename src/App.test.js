0d4a746ea11f49cde785227037d92c64b7fa7633
ceofred <johnsonmessilo19@gmail.com>
2019-11-05 04:07:36 +0100

    fix directory for test

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
-import store from './redux/store';
+import store from './store/index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
