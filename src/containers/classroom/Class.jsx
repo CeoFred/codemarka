a5ccb52e438d2502b91be04aed97128b80795d85
ceofred <johnsonmessilo19@gmail.com>
2019-11-24 14:43:42 +0100

    fix chat

--- src/containers/classroom/Class.jsx
+++ src/containers/classroom/Class.jsx
@@ -1,122 +1,161 @@
-import React,{useState} from 'react'
-// import io from "socket.io-client";
+import React, { useState } from "react";
+import io from "socket.io-client";
 
 import Navigation from "../../components/classroom/UI/NavBar";
 import Convo from "./Conversation";
 // import Editor from "../../components/classroom/Editor/Editor";
 
-const MainClassLayout = ({user_id,classroom_id}) => {
 
-    const [inputState, setInputState] = useState({
-        value:'',
-        isFocused:false
-    });
+const socket = io("http://localhost:2001/classrooms");
 
-    // const [inRoom, setInRoom] = useState(false);
+const MainClassLayout = ({ data }) => {
+  const [inputState, setInputState] = useState({
+    value: "",
+    isFocused: false,
+    lastSentMessage:null,
+  });
 
-    // React.useEffect(() => {
-    //     const socket = io("http://localhost:2001/classrooms");
+  const [colabstate, setColabState] = useState({
+      messages:[]
+  });
+  const [inRoom, setInRoom] = useState(false);
+
+  React.useEffect(() => {
+
+
+      console.log('Room status - ',inRoom);
+
+      const requestData = {
+        classroom_id:data.classroom_id,
+        user:data.user_id
+        };  
+
+      if(inRoom !== true && inRoom !== null){
+       // set listeners and emitters
+
+     
+        // tell server to add user to class
+        socket.emit('join',requestData);
+        console.log('You joined');
         
-    //     console.log('Room status - ',inRoom)
         
-    //     const requestData = {
-    //       classroom_id,
-    //       user:user_id
-    //     }
 
-    //     if(inRoom === true){ // user requested to join the class
+        //listen for new members added
+        socket.on('someoneJoined',(from,msg,user) => {
+          setColabState((c) =>{
+            let oldmsg = c.messages;
+            oldmsg.push({from,msg,user})
+            return {...c,messages:oldmsg}
+          });
+        });
+
+
+        //listen for old message
+        socket.on('updateMsg',(who,msg) => {
+          setColabState((c) =>{
+            return {...c,messages:msg}
+          });
+        });
+
+
+        //listen for new messages
+        socket.on('nM',(data) => {
           
-    //       // tell server to add user to class
-    //       socket.emit('aRequestToAddUser',requestData)
-    //       console.log('You joined')
-          
-    //       //listen for new members added
-    //       socket.on('someoneJoined',(who,msg) => {
-    //         setColabState((c) =>{
-    //           let oldmsg = c.messages;
-    //           oldmsg.push({from:who,msg})
-    //           return {...c,messages:oldmsg}
-    //         })
-    //       })
-    //       //listen for old message
-    //       socket.on('updateMsg',(who,msg) => {
-    //         setColabState((c) =>{
-    //           return {...c,messages:msg}
-    //         })
-    //       })
-          
-    //       //listen for new messages
-    
-    //       //listen for members leaving
-    //       socket.on('updatechat_left',(who,msg) => {
-    //         setColabState((c) =>{
-    //           let oldmsg = c.messages;
-    //           oldmsg.push({from:who,msg})
-    //           return {...c,messages:oldmsg}
-    //         })
-    //       })
-    
-    //     } else if(inRoom === null) {
-    //       console.log('You have not joined')
-    //     } else if(inRoom === false){
-    //       console.log('You left')
-    //     }
-    //     return () => {
-    //       console.log('return function',inRoom)
-    //       if (inRoom) {
-    //         console.log("Leaving room");
-    //         socket.emit("leave", requestData);
-            
-    //         setColabState((c) =>{
-    //           let oldmsg = c.messages;
-    //           oldmsg.push({from:'self',msg:'you left'})
-    //           return {...c,messages:oldmsg}
-    //         })
-    //         dispatch(action.userLeftAClass(colabstate.classroom_id));
-    //       };
-    //     }
-    //   },[dispatch,inRoom,colabstate.username,colabstate.classroom_id]);
-    const handleInputChange = (e) => {
-        e.preventDefault();
-        const value = e.target.value;
-        setInputState({...inputState,value})
-    }
+          setColabState((c) =>{
+            let oldmsg = c.messages;
+            oldmsg.push({from:data.user,msg: data.message})
+            return {...c,messages:oldmsg}
+          });
 
-    const handleMessageSubmit = () => {
-        console.log('Message was sent');
-    }
+        });
 
-    const userLeftInput = (e) => {
-        e.preventDefault();
 
-        console.log('User left input')
-    }
+        //listen for members leaving
+        socket.on('updatechat_left',(who,msg) => {
+          setColabState((c) =>{
+            let oldmsg = c.messages;
+            oldmsg.push({from:who,msg})
+            return {...c,messages:oldmsg}
+          });
+        });
 
-    const userEnteredInput = (e) => {
-        e.preventDefault();
-        console.log('User entered input')
-    }
+      } else if(inRoom === null) {
+        console.log('You have not joined')
+      } else if(inRoom === false){
+        console.log('You left')
+      }
 
-    return (
-        <div>
-        <Navigation classid={classroom_id}/>
-            <div style={{width:'100%',height:'87vh'}}>
-                <Convo 
-                inputValue={inputState}
-                handleInputChange={handleInputChange}
-                sendMessage={handleMessageSubmit}
-                inputBlur={userLeftInput}
-                inputFocused={userEnteredInput}
-                />
+      return () => {
+        console.log('return function',inRoom)
+        if (inRoom) {
+          console.log("Leaving room");
+          socket.emit("leave", requestData);
 
-                <div style={{float:'left',width:'60%',height:'100%'}}>
-                {/* <Editor /> */}
-                Editor Here
-                </div>
-                
-            </div>
+          setColabState((c) =>{
+            let oldmsg = c.messages;
+            oldmsg.push({from:'self',msg:'you left'})
+            return {...c,messages:oldmsg}
+          });
+        //   dispatch(action.userLeftAClass(colabstate.classroom_id));
+        };
+      }
+    },[inRoom,colabstate.username,colabstate.classroom_id]);
+
+  const handleInputChange = e => {
+    e.preventDefault();
+    const value = e.target.value;
+    setInputState({ ...inputState, value });
+  };
+
+  const handleMessageSubmit = (e) => {
+      e.preventDefault();
+
+      setInputState({...inputState,
+        lastSentMessage:e.target.value,
+        value:''});
+
+        const msg_data = {
+          user:data.user_id,
+          class:data.classroom_id,
+          message:inputState.value
+        }
+        console.log(msg_data);
+
+        socket.emit('newMessage',msg_data)
+  };
+
+  const userLeftInput = e => {
+    e.preventDefault();
+    setInputState({ ...inputState, isFocused: false });
+  };
+
+  const userEnteredInput = e => {
+    e.preventDefault();
+    setInputState({ ...inputState, isFocused: true });
+  };
+
+  return (
+    <div>
+      <Navigation classid={data.classroom_id} />
+      <div style={{ width: "100%", height: "87vh" }}>
+        <Convo
+          inputValue={inputState.value}
+          handleInputChange={handleInputChange}
+          sendMessage={(e) => handleMessageSubmit(e)}
+          inputBlur={userLeftInput}
+          inputFocused={userEnteredInput}
+          focused={inputState.isFocused}
+          messages={colabstate.messages}
+          user={data.user_id}
+        />
+
+        <div style={{ float: "left", width: "60%", height: "100%" }}>
+          {/* <Editor /> */}
+          Editor Here
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default MainClassLayout
+export default MainClassLayout;
--- src/containers/classroom/Conversation.jsx
+++ src/containers/classroom/Conversation.jsx
@@ -1,36 +1,61 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import './css/conversation.css';
+import "./css/conversation.css";
 
 export default function Conversation(props) {
-    document.addEventListener('keyup',(e) => {
-        if(e.key === 'Enter'){
-            props.sendMessage()
-        }
-    })
-    return (
-        <div className="conversation__container">
-            {/* messages tab */}
-        <div className="container bg-black messages">
-            <div className="message">
-            <span className="message__username">codemon</span>
-            <div className="message_content">
-                Lorem, ipsum dolor sit amet consectetur
-                 adipisicing elit. 
-            </div>
-            </div>
-        </div>
+ const inputKeyDown = (event) => {
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        props.sendMessage(event);
+      }
+ }
 
-            {/* input text area */}
-        <div className="input_container">
-        <textarea
-        onBlur={props.inputBlur}
-        onFocus={props.inputFocused}
-         value={props.inputValue}
-          onChange={props.handleInputChange} 
-          placeholder="Write a message" 
-          className="form-control"></textarea>
-            </div>  
+ let messages = props.messages.map(m => {
+   if((m.from).toLowerCase() === 'server'){
+     if(m.user === props.user){
+       return (
+         
+       <div>
+       <span className="message__username text-info">You Joined</span>
         </div>
-    )
+       )
+     }
+     return (
+       <div>
+      <span className="message__username text-info">{m.msg}</span>
+       </div>
+     )
+   }else {
+    return (
+      <div className="message" key={m.index}>
+      <span className="message__username">{m.from}</span>
+     <div className="message_content">{m.msg}</div>
+    </div>
+     )
+   }
+   
+ })
+  return (
+    <div className="conversation__container">
+      {/* messages tab */}
+      <div className="container bg-black messages">
+      {messages}
+      </div>
+
+      {/* input text area */}
+      <div className="input_container">
+        <textarea
+          id="input_area"
+          onBlur={props.inputBlur}
+          onFocus={props.inputFocused}
+          value={props.inputValue}
+          onChange={props.handleInputChange}
+          onKeyDown={inputKeyDown}
+          placeholder="Write a message"
+          className="form-control"
+        ></textarea>
+      </div>
+    </div>
+  );
 }
