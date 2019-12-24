81e2557bafc1c72ec1e806fb49de9775715e8c7f
ceofred <johnsonmessilo19@gmail.com>
2019-12-24 09:44:18 +0100

    make navbar overlap

--- src/components/UI/Navigation/NavigationBar.jsx
+++ src/components/UI/Navigation/NavigationBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import NavigationBarItems from "./NavigationBarItems";
-import { useSelector } from "react-redux";
+import NavigationBarItems from './NavigationBarItems';
+import { useSelector } from 'react-redux';
 
 import logo from '../../../media/images/colab-04.png'
 export default function NavigationBar() {
@@ -17,7 +17,7 @@ export default function NavigationBar() {
   
     return (
         <nav className="navbar navbar-horizontal navbar-expand-lg navbar-dark navbar-transparent" 
-style={ { backgroundColor:'#172839',display:display ? 'block' : 'none' } }>
+style={ { backgroundColor:'#172839',display:display ? 'block' : 'none' ,zIndex:9999,position:'absolute',width:'100%',top:0} }>
             <div className="container">
                 <Link className="navbar-brand" to="/"><img height="30px" src={ logo } alt='logo'/></Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-primary" aria-controls="navbar-primary" aria-expanded="false" aria-label="Toggle navigation">
