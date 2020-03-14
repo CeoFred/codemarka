c8c2b0b5b8b5d93464d0995edf9fc3a46c1c4bc5
ceofred <johnsonmessilo19@gmail.com>
2020-03-14 18:59:27 +0100

    removed warning logs

--- src/containers/auth/ChangePassword.jsx
+++ src/containers/auth/ChangePassword.jsx
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react';
+import React,{ useState } from 'react';
 
 import Button from '../../components/Partials/Button';
 import Input from '../../components/Partials/Input';
@@ -18,24 +18,6 @@ const initialPrependsvg = (
     class="feather feather-key"
   >
         <path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.777-7.777zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4" />
-    </svg>
-);
-
-const finalAppendsvg = (
-    <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    class="feather feather-eye"
-  >
-        <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-        <circle cx="12" cy="12" r="3" />
     </svg>
 );
 
