0c3672609bbbbb6ac4107a7af320a5ac0427abef
ceofred <johnsonmessilo19@gmail.com>
2020-04-26 18:19:40 +0100

    fast fidex

--- src/components/Partials/SearchContainer.jsx
+++ src/components/Partials/SearchContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 function SearchContainer({ results, display }) {
   let res;
 
@@ -8,9 +8,9 @@ function SearchContainer({ results, display }) {
       return (
           <div className="list p-0 text-align-left" key={ r.kid }>
           { r.name.toLowerCase() === 'no results found!!' ? (<b>Whoops! No result found</b>) : 
-              (<div><a href={ `/c/classroom/${ r.kid }` } className="text-dark">
+              (<div><Link to={ `/c/classroom/${ r.kid }` } className="text-dark">
                 {r.topic}
-              </a>
+              </Link>
             <p><small>by: {r.name.toLowerCase() || r.communityName.toLowerCase()}</small></p></div>)
             }
           </div>
@@ -18,8 +18,8 @@ function SearchContainer({ results, display }) {
     });
   }
   return (
-      <div className={ `card ${ display ? 'd-block' : 'd-none' }` }>
-          <div className="card-body border-r">{res}</div>
+      <div className={ `card ${ display ? 'd-block' : 'd-none' } h-50` }>
+      <div className="card-body border-r h-100 overflow-scroll">{res}</div>
       </div>
   );
 }
